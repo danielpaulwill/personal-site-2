e9217b59636fd2ffbc8e32d16df287c3f916d520
Daniel Williams <danielpaulwill@gmail.com>
2021-07-17 01:16:59 -0400

    still trying to get it to work

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-/*
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-document.querySelector('body').appendChild(h2); 
-*/
+const update = document.getElementById("update"); 
+
+
+
+function moveUpdateLeft() {
+    const leftNumbers = update.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      update.style.left = `${left - 10}px`;
+    }
+  } 
+
+
+  function moveUpdateRight() {
+      const leftNumbers = update.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+
+      if (left < 1000) {
+          update.style.left = `${left + 10}px`;
+      }
+  }
+
+
+
+
+  
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveUpdateLeft();
+      console.log(moveUpdateLeft);
+    }
+    if (e.key === "ArrowRight") {
+        moveUpdateRight();
+        console.log();
+    }
+  }); 
+  
+  
+  
+
 
